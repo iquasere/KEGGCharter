07a913667dd86ea7ebd6edf7a5786f4b0b55940a
iquasere <maildosequeira@gmail.com>
2024-01-02 16:23:13 +0000

    Fix on reducing ids_xrefs

--- keggcharter.py
+++ keggcharter.py
@@ -368,7 +368,7 @@ def ids_xref(
     data = pd.merge(data, new_ids, on=f'{in_col}_split', how='left')
     del data[f'{in_col}_split']
     result = data.groupby([col for col in data.columns if col != out_col], as_index=False).agg({
-        out_col: lambda x: ','.join(set(x))})
+        out_col: lambda x: ','.join(set(x) if type(x) != float else x)})
     return result
 
 
